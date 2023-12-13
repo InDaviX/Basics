ba2f5a4f7fdde63dd482b916200c804f87581df5
InDaviX <dszewczyk235@gmail.com>
2023-12-13 15:13:46 +0100

    Właściwości i ich typy

--- App.js
+++ App.js
@@ -1,23 +1,8 @@
 import React from 'react';
 import {SafeAreaView, View, Text} from 'react-native';
-import MyText from './components/MyText/MyText';
 
 const App = () => {
-  return (
-    <SafeAreaView>
-      <View>
-        <Text>This is going to be our header</Text>
-      </View>
-      <View>
-        <MyText />
-        <MyText />
-        <MyText />
-      </View>
-      <View>
-        <Text>All rights reserved</Text>
-      </View>
-    </SafeAreaView>
-  );
+  return <SafeAreaView />;
 };
 
 export default App;
